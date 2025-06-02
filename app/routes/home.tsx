4c58c24ed8b58c54181c0b34bd1282ad73fd3f14
Luke Skelhorn <luke.skelhorn@gmail.com>
2025-06-02 08:15:44 +0100

    Updated description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import { Welcome } from "../welcome/welcome";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Skel Tech" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Skel Tech | Home" },
   ];
 }
 
